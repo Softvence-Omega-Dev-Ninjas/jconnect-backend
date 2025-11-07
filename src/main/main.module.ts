72894f4cfa50222ccfe04ba2ce7cbddac58391a1
shamim Rana <shamimranaprofessional.office@gmail.com>
2025-11-08 00:56:12 +0600

    conflict solved

--- src/main/main.module.ts
+++ src/main/main.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
+import { ProfileModule } from "./profile/profile.module";
 import { ServiceModule } from "./service/service.module";
 import { UsersModule } from "./users/users.module";
 
 @Module({
-    imports: [AuthModule, UsersModule, ServiceModule],
+    imports: [AuthModule, UsersModule, ServiceModule, ProfileModule],
 })
 export class MainModule {}
