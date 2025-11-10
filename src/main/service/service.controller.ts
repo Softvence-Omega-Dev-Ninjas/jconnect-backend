7a5a4aca01a287217b9c804529c4693504afc93e
masud2005 <masud20012005@gmail.com>
2025-11-11 05:47:02 +0600

    service controller update

--- src/main/service/service.controller.ts
+++ src/main/service/service.controller.ts
@@ -1,4 +1,4 @@
-import { ValidateArtist, ValidateUser } from "@common/jwt/jwt.decorator";
+import { GetUser, ValidateArtist, ValidateUser } from "@common/jwt/jwt.decorator";
 import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Service } from "@prisma/client";
@@ -35,7 +35,7 @@ export class ServiceController {
     @ApiOperation({ summary: "Get service details by ID" })
     @ApiResponse({ status: 200, description: "Service details found" })
     @ApiResponse({ status: 404, description: "Service not found" })
-    findOne(@Param("id") id: string): Promise<Service> {
+    findOne(@Param("id") id: string, @GetUser() user: any) {
         return this.serviceService.findOne(id);
     }
 
@@ -45,8 +45,12 @@ export class ServiceController {
     @ApiOperation({ summary: "Update service details by ID" })
     @ApiResponse({ status: 200, description: "Service updated successfully" })
     @ApiResponse({ status: 404, description: "Service not found" })
-    update(@Param("id") id: string, @Body() updateServiceDto: UpdateServiceDto): Promise<Service> {
-        return this.serviceService.update(id, updateServiceDto);
+    update(
+        @Param("id") id: string,
+        @GetUser() user: any,
+        @Body() updateServiceDto: UpdateServiceDto,
+    ): Promise<Service> {
+        return this.serviceService.update(id, user, updateServiceDto);
     }
 
     @ApiBearerAuth()
--- src/main/service/service.service.ts
+++ src/main/service/service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
 import { Service } from "@prisma/client";
 import { PrismaService } from "src/lib/prisma/prisma.service";
 import { CreateServiceDto } from "./dto/create-service.dto";
@@ -25,11 +25,28 @@ export class ServiceService {
         if (!service) {
             throw new NotFoundException(`Service with ID ${id} not found`);
         }
-
         return service;
     }
 
-    async update(id: string, updateServiceDto: UpdateServiceDto): Promise<Service> {
+    async update(id: string, user, updateServiceDto: UpdateServiceDto): Promise<Service> {
+        console.log(user);
+        const service = await this.prisma.service.findUnique({
+            where: { id },
+        });
+
+        if (!service) {
+            throw new NotFoundException(`Service with ID ${id} not found`);
+        }
+
+        // check ownership or super admin
+        const isOwner = service.creatorId === user?.userId;
+        const isSuperAdmin = user?.roles === "SUPER_ADMIN";
+
+        if (!isOwner && !isSuperAdmin) {
+            console.log("You are not authorized to access this service");
+            throw new ForbiddenException("You are not authorized to access this service");
+        }
+
         return this.prisma.service.update({
             where: { id },
             data: updateServiceDto,
