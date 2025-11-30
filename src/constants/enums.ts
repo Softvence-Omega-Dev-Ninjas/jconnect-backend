952f89b81be58a31ee5bc5030fb795f743368d4e
shamim Rana <shamimranaprofessional.office@gmail.com>
2025-12-01 00:19:08 +0600

    change enum

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -1,2 +1,26 @@
-export const test = ["a", "b", "c"] as const;
-export type Test = (typeof test)[number];
+export const validationType = [
+    "EMAIL",
+    "PHONE"
+] as const;
+export type ValidationType = typeof validationType[number];
+
+// AuthProvider as string constants
+export const authProvider = [
+    "GOOGLE",
+    "FACEBOOK"
+] as const;
+export type AuthProvider = typeof authProvider[number];
+
+
+export const role = [
+    "USER",
+    "MODERATOR",
+    "ADMIN",
+    "SUPER_ADMIN",
+    "FINANCE_ADMIN",
+    "SUPPORT_ADMIN",
+    "ANALYST",
+    "MEMBER",
+    "ARTIST"
+] as const
+export type Role = typeof role[number]
