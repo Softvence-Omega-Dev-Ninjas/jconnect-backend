f63c089ed1c92a564fbdb90098b05735b5819a1f
Joy43 <ssjoy43@gmail.com>
2025-12-16 02:18:07 +0600

    annoucnement crate

--- src/main/settings/settings.controller.ts
+++ src/main/settings/settings.controller.ts
@@ -1,16 +1,17 @@
-import { Body, Controller, Get, Patch } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { SettingsService } from "./settings.service";
 
 import { GetUser, ValidateAdmin, ValidateUser } from "@common/jwt/jwt.decorator";
+import { Announcement } from "./dto/announcement.dto";
 import { UpdateSettingDto } from "./dto/create-dto";
 
 @ApiTags("settings")
-@ApiBearerAuth()
+
 @Controller("settings")
 export class SettingsController {
-    constructor(private readonly settingsService: SettingsService) {}
-
+    constructor(private readonly settingsService: SettingsService) { }
+    @ApiBearerAuth()
     @ValidateUser()
     @Get()
     @ApiOperation({ summary: "Get platform settings" })
@@ -19,6 +20,7 @@ export class SettingsController {
     }
 
     @ValidateAdmin()
+    @ApiBearerAuth()
     @Patch()
     @ApiOperation({ summary: "Update platform settings" })
     update(@Body() dto: UpdateSettingDto) {
@@ -27,9 +29,44 @@ export class SettingsController {
 
     // ----------------- notification settings ---------------------
     @ValidateAdmin()
+    @ApiBearerAuth()
     @Patch("notification-toggle-settings-only-admin")
     @ApiOperation({ summary: "Update notification settings for only admin" })
     updateNotificationSettingsToggleAdmin(@GetUser("userId") userId: string) {
         return this.settingsService.updateNotificationSettingsToggleAdmin(userId);
+    }
+
+    // ---------------announcement create-------------
+
+    @ValidateAdmin()
+    @ApiBearerAuth()
+    @Post("announcement")
+    @ApiOperation({ summary: "Create announcement" })
+    createAnnouncement(@Body() dto: Announcement) {
+        return this.settingsService.createAnnouncement(dto);
+    }
+    // ---------get announcement----------------
+    @Get("announcement")
+    @ApiOperation({ summary: "Get announcement" })
+    getAnnouncement() {
+        return this.settingsService.getAnnouncement();
+    }
+
+    // ---------------announcement delete-------------
+    @ValidateAdmin()
+    @ApiBearerAuth()
+    @Delete("announcement/:id")
+    @ApiOperation({ summary: "Delete announcement" })
+    deleteAnnouncement(@Param("id") id: string) {
+        return this.settingsService.deleteAnnouncement(id);
+    }
+
+    // ---------------announcement update-------------
+    @ValidateAdmin()
+    @ApiBearerAuth()
+    @Patch("announcement/:id")
+    @ApiOperation({ summary: "Update announcement" })
+    updateAnnouncement(@Param("id") id: string, @Body() dto: Announcement) {
+        return this.settingsService.updateAnnouncement(id, dto);
     }
 }
--- src/main/settings/settings.service.ts
+++ src/main/settings/settings.service.ts
@@ -1,11 +1,13 @@
 import { HandleError } from "@common/error/handle-error.decorator";
+import { successResponse } from "@common/utilsResponse/response.util";
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/lib/prisma/prisma.service";
+import { Announcement } from "./dto/announcement.dto";
 import { UpdateSettingDto } from "./dto/create-dto";
 
 @Injectable()
 export class SettingsService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
     async getSettings() {
         const settings = await this.prisma.setting.findUnique({
@@ -49,4 +51,51 @@ export class SettingsService {
             data: user.notificationToggles,
         };
     }
+
+    // -------------------createAnnouncement------------
+    @HandleError("Failed to create announcement")
+    async createAnnouncement(dto: Announcement) {
+        const announcement = await this.prisma.announcement.create({
+            data: {
+                title: dto.title,
+                description: dto.description,
+            },
+        })
+
+        return successResponse(announcement, "Announcement created");
+    }
+
+    @HandleError("Failed to get announcement")
+    async getAnnouncement() {
+        const announcement = await this.prisma.announcement.findMany();
+        return successResponse(announcement, "Announcement fetched");
+    }
+
+    //  deleteAnnouncement
+
+    @HandleError("Failed to delete announcement")
+    async deleteAnnouncement(id: string) {
+        const announcement = await this.prisma.announcement.delete({
+            where: {
+                id: id,
+            },
+        });
+        return successResponse(announcement, "Announcement deleted");
+    }
+
+    // ------------updateAnnouncement---
+    @HandleError("Failed to update announcement")
+    async updateAnnouncement(id: string, dto: Announcement) {
+        const announcement = await this.prisma.announcement.update({
+            where: {
+                id: id,
+            },
+            data: {
+                title: dto.title,
+                description: dto.description,
+            },
+        });
+        return successResponse(announcement, "Announcement updated");
+    }
+
 }
