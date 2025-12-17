360a851cabc5205d905af015b5d7d5e08e82a369
masud2005 <masud20012005@gmail.com>
2025-12-18 00:03:00 +0600

    social profile bug fix from profile module

--- src/main/profile/social-profile.controller.ts
+++ src/main/profile/social-profile.controller.ts
@@ -1,15 +1,15 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { GetUser, ValidateUser } from "@common/jwt/jwt.decorator";
-import { SocialProfileService } from "./social-profile.service";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { CreateSocialProfileDto, UpdateSocialProfileDto } from "./dto/social-profile.dto";
+import { SocialProfileService } from "./social-profile.service";
 
 @ApiTags("Social Profile")
 @ApiBearerAuth()
 @ValidateUser()
 @Controller("social-profiles")
 export class SocialProfileController {
-    constructor(private readonly socialProfileService: SocialProfileService) {}
+    constructor(private readonly socialProfileService: SocialProfileService) { }
 
     @Post()
     @ApiOperation({ summary: "Add a new social profile link" })
@@ -23,13 +23,7 @@ export class SocialProfileController {
         return this.socialProfileService.findAll(user.userId);
     }
 
-    @Get(":id")
-    @ApiOperation({ summary: "Get a specific social profile link" })
-    async findOne(@GetUser() user: any, @Param("id") id: string) {
-        return this.socialProfileService.findOne(user.userId, id);
-    }
-
-    @Put(":id")
+    @Patch(":id")
     @ApiOperation({ summary: "Update a social profile link" })
     async update(
         @GetUser() user: any,
