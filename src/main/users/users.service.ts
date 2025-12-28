e26695adbd0068c4225a671f029923b4c18d83e0
shamimrana2006 <shamimranaprofessional.office@gmail.com>
2025-12-29 00:06:28 +0600

    feat: update findAllArtist method to include user verification and modify price column data type in migrations

--- src/main/users/users.service.ts
+++ src/main/users/users.service.ts
@@ -409,12 +409,12 @@ export class UsersService {
                 const avgA =
                     a.ReviewsReceived.length > 0
                         ? a.ReviewsReceived.reduce((sum, r) => sum + r.rating, 0) /
-                          a.ReviewsReceived.length
+                        a.ReviewsReceived.length
                         : 0;
                 const avgB =
                     b.ReviewsReceived.length > 0
                         ? b.ReviewsReceived.reduce((sum, r) => sum + r.rating, 0) /
-                          b.ReviewsReceived.length
+                        b.ReviewsReceived.length
                         : 0;
                 return avgB - avgA;
             });
