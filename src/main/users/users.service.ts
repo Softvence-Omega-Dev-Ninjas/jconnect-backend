a1afc56a5997d7077fb95445e1f31fdbc94602a0
shamimrana2006 <shamimranaprofessional.office@gmail.com>
2026-01-08 05:40:58 +0600

    update some feature immediately push

--- src/main/users/users.service.ts
+++ src/main/users/users.service.ts
@@ -384,7 +384,9 @@ export class UsersService {
             isDeleted: false,
             isActive: true,
             isVerified: true,
-            // role: Role.ARTIST,
+            role: {
+                notIn: [Role.ADMIN, Role.SUPER_ADMIN],
+            },
             // id: { not: user.userId },
         };
 
